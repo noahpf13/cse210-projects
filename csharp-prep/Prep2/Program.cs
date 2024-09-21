519eb443186f7495a67389ba44347f14e95555fc
npforero13 <115837581+npforero13@users.noreply.github.com>
2024-09-21 13:44:48 -0600

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
+      //get user's grade percentage, convert to int
+      Console.Write("What is your grade percentage? ");
+      string percentage = Console.ReadLine();
+
+      
+      int grade = int.Parse(percentage);
+
+      string sign = "+";
+
+    
+
+
+      if (grade >= 90)
+      {
+        if (grade % 10 >= 7)
+        {
+            
+        }
         
+      }
+      else if (grade >= 80)
+      {
+        Console.WriteLine("Your grade is B");
+      }
+      else if (grade >= 70)
+      {
+        Console.WriteLine($"Your grade is C{sign}");
+      }
+      else if (grade >= 60)
+      {
+        Console.WriteLine("Your grade is D");
+      }
+      else
+      {
+        Console.WriteLine("Your grade is F");
+      }
     }
 }
