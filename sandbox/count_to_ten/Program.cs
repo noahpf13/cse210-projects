34580f910e3de89e98e3a6c33cf05f35b7a9097c
npforero13 <115837581+npforero13@users.noreply.github.com>
2024-09-23 13:40:35 -0600

    built new project

--- sandbox/count_to_ten/Program.cs
+++ sandbox/count_to_ten/Program.cs
@@ -1,10 +1,10 @@
-using System;
+﻿namespace count_to_ten;
 
 class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 1; i <= 10; i += 1)
         {
             Console.WriteLine(i);
         }
